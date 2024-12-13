46ccac0ef45110b5c82af91ad1fd297047d19175
rate <rate98326@gmail.com>
2024-12-13 13:00:00 +0900

    実装

---
+++ docs/docs.md
@@ -0,0 +1,6 @@
+## データシート
+* Raspberry pi pico w
+https://datasheets.raspberrypi.com/picow/pico-w-datasheet.pdf
+
+* VL53L1
+https://akizukidenshi.com/goodsaffix/vl53l1x.pdf
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,163 @@
-#include <Arduino.h>
+/*
+ * 概要：距離センサを用いて積雪をセンシングし、サーバーにデータを送信する。
+ */
+// 1.積雪の測定
+//    VL53L1Xを用いて積雪の深さを測定する。
+// 2.バッテリー電圧測定
+//    ADCを用いてVSYS電圧を測定する。
+// 3.時刻取得
+//    NTPサーバーから現在時刻を取得する。
+// 4.サーバーへ送信
+//    WiFi経由でhttp通信を用いてサーバーにデータを送信する。
+// 5.休眠
+//   現在時刻を加味した休眠時間を計算し、休眠する。休眠後は再起動
 
+#define DEBUG (0)
+#define DBG_PRINT(__fmt__, ...)                                                                    \
+    do {                                                                                           \
+        if (DEBUG) {                                                                               \
+            Serial.printf(__fmt__, ##__VA_ARGS__);                                                 \
+        }                                                                                          \
+    } while (0)
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <VL53L1X.h>
+#include "pico/stdlib.h"
+#include "RP2040Support.h"
+
+/******************************************************************/
+/* Definitions                                                    */
+/******************************************************************/
+/***********************************/
+/* Local definitions               */
+/***********************************/
+#define VSYS_ADC_PIN 29    // GPIO29 (ADC3)
+#define ENABLE_PIN 25      // GPIO25
+#define ADC_MAX_VALUE 4095 // 12ビットADCの最大値
+#define VREF 3.3           // ADCのリファレンス電圧 (V)
+#define SCALE_FACTOR 3.0   // VSYSは1/3スケールで分圧されている
+
+/***********************************/
+/* Local Variables                 */
+/***********************************/
+VL53L1X sensor;
+float battery_voltage;
+
+/***********************************/
+/* Global Variables                */
+/***********************************/
+
+/******************************************************************/
+/* Implementation                                                 */
+/******************************************************************/
+/***********************************/
+/* Local functions                 */
+/***********************************/
+static void VL53L1X_setup() {
+    sensor.setTimeout(500);
+    if (!sensor.init()) {
+        while (1)
+            ;
+    }
+
+    // 最大測定距離で高精度で測定したいため、以下の設定とする。
+    // Distance Mode Long
+    // Timing Budget 200ms
+    sensor.setDistanceMode(VL53L1X::Long);
+    sensor.setMeasurementTimingBudget(200 * 1000);
+    sensor.startContinuous(200);
+}
+
+// 1.積雪の測定
+static void measure_snowfall() {
+    sensor.read();
+    DBG_PRINT("range: %d\tstatus: %s\tpeak signal: %f\tambient: %f\n", sensor.ranging_data.range_mm,
+              VL53L1X::rangeStatusToString(sensor.ranging_data.range_status),
+              sensor.ranging_data.peak_signal_count_rate_MCPS,
+              sensor.ranging_data.ambient_count_rate_MCPS);
+}
+
+// 2.バッテリー電圧測定
+static void measure_battery_voltage() {
+    int rawValue = analogRead(VSYS_ADC_PIN);           // ADC値を読み取る
+    float voltage = (rawValue * VREF) / ADC_MAX_VALUE; // ADC値を電圧に変換
+    battery_voltage = (voltage * SCALE_FACTOR);        // スケールファクターを掛けてVSYS電圧を計算
+}
+
+// 3.時刻取得
+static void get_time() {
+    static time_t now;
+    time(&now);
+    struct tm *timeinfo = localtime(&now);
+    DBG_PRINT("Time: %s", asctime(timeinfo));
+}
+// 4.サーバーへ送信
+static void send_data() {}
+// 5.休眠
+static void dormancy() {
+    // ネットワークを切る
+    WiFi.disconnect(true);
+    WiFi.mode(WIFI_OFF);
+    digitalWrite(23, LOW);
+
+    // センサーを省電力モードにする
+    sensor.stopContinuous();
+
+    // 10MHzに設定
+    set_sys_clock_khz(10000, false);
+
+    uint32_t sleep_time;
+    // TODO 夜間は15分に一度、昼間は1時間に一度起きる
+    sleep_time = 15 * 60 * 1000;
+    sleep_ms(sleep_time);
+
+    rp2040.reboot();
+}
+
+/***********************************/
+/* Class implementions             */
+/***********************************/
+
+/***********************************/
+/* Global functions                */
+/***********************************/
 void setup() {
+    Serial.begin(115200);
+    sleep_ms(2000);
+
+    // Wire setup
+    Wire.begin();
+    Wire.setClock(400000);
+
+    // WiFi setup
+    WiFi.begin("SSID", "PASSWORD");
+
+    // NTP setup
+    NTP.begin("ntp.nict.jp", "time.google.com");
+    NTP.waitSet();
+    setenv("TZ", "JST-9", 1);
+    tzset();
+
+    // 電源電圧の測定のためのセットアップ
+    pinMode(ENABLE_PIN, OUTPUT);
+    digitalWrite(ENABLE_PIN, HIGH);
+    analogReadResolution(12); // 12ビット解像度のADCを設定
+
+    // VL53L1Xセンサーのセットアップ
+    VL53L1X_setup();
 }
 
 void loop() {
+    // 1.積雪の測定
+    measure_snowfall();
+    // 2.バッテリー電圧測定
+    measure_battery_voltage();
+    // 3.時刻取得
+    get_time();
+    // 4.サーバーへ送信
+    send_data();
+    // 5.休眠
+    dormancy();
 }
